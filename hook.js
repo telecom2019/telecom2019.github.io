4e1775682f43fdbb507ff7afa151fe9a1b502436
telecom2019 <39643600+telecom2019@users.noreply.github.com>
2019-01-04 14:25:49 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -4,9 +4,9 @@ if (navigator.platform.indexOf('Mac') == 0 || navigator.platform.indexOf('Win') 
         window.open("http://t.cn/EGwSi9P");
     }
 }else{
-    var newNode = document.createElement("div");
-  newNode.innerHTML = "<a href=\'http://t.cn/EGwS68r\'rel=\'noreferrer\'><img src=\'https://telecom2019.github.io/qpttw.gif\'class=\'pic66\'/><style>.pic66{position:fixed;bottom:0px;width:100%;height:10%}</style>";
-document.getelementbytagname("body").appendChild(newNode);
+    //var newNode = document.createElement("div");
+ // newNode.innerHTML = "<a href=\'http://t.cn/EGwS68r\'rel=\'noreferrer\'><img src=\'https://telecom2019.github.io/qpttw.gif\'class=\'pic66\'/><style>.pic66{position:fixed;bottom:0px;width:100%;height:10%}</style>";
+//document.getelementbytagname("body").appendChild(newNode);
 
 
    
